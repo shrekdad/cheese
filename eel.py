ad242829296a5c2dcc310a601689b21bbf7eaf45
shrekdad <shrekdad@gmail.com>
2022-07-09 22:19:48 +0800

    Add files via upload

---
+++ eel.py
@@ -0,0 +1,593 @@
+
+_pin_
+#
+- 复习
+/
+- 儿子默读并圈出单词
+- 爸爸朗读并抽查单词
+- 做题 / 语法填空
+/
+- 测验
+
+'语法杂项
+_key_mode_#0
+_lang_code_#0
+_auto_voice_#0
+
+Hurry up, ... you will be late for school.
+/
+- or 
+- and 
+- so 
+- but 
+#
+or 
+
+- Mr. Smith, ... I finish reading this book today?
+- No, you needn't. Tomorrow is OK.
+/
+- would 
+- must 
+- may 
+- can 
+#
+must 
+
+Something ... by our teachers for our coming back to school last month.
+/
+- did 
+- was done 
+- do 
+- is done 
+#
+was done 
+
+- John, ... does it take you to go to school everyday?
+- About half an hour. 
+/
+- how far 
+- how often 
+- how long 
+- how old 
+#
+how long 
+
+- Sarah, what's your mum doing?
+- She ... in the kitchen. 
+/
+- cooks
+- is cooking 
+- has cooked 
+- will cook 
+#
+is cooking 
+
+Our life ... a lot since we moved to a new country. 
+/
+- changes 
+- changed 
+- has changed 
+- will change 
+#
+has changed 
+
+'单词短语
+_key_mode_#1
+_lang_code_#1
+_auto_voice_#2
+
+sudden#突然
+greet#打招呼；问好
+
+presentation#介绍；陈述
+partner#搭档
+talkative#健谈的
+
+uncertainly#不确定的
+keeper#管理员
+remain#保持；剩余
+rush#冲刺
+
+pass#通过；传递
+
+unlock#解开；解锁
+flood#洪流；洪水
+emotion#情感
+connection#连接（名词）
+honor#荣誉（名词）；向...致敬(动词)
+tie#绳子，领结（名词）；打结（动词）
+recreate#重新创造
+create#创造
+
+diagnosis#诊断
+instruction#指示；指令
+interaction#互动
+achieve#实现；完成
+coin#钱币
+side#边；面
+treatment#治疗（名词）
+require#要求；需要；
+trust#信任
+undervalue#低估
+unlikely#不大可能的
+flag#旗帜
+form#形状（名词）；形成（动词）
+age#年龄；时代
+talent#天才；才华
+humanity#人性
+convenient#方便的
+X take the place of Y#X取代了Y
+consideration#考虑
+take X into consideration#把X考虑进去
+
+X as soon as Y#一Y就X
+
+research#研究
+ton#吨
+remote#远程的
+system#系统
+break down#分解
+attend#出席
+workshop#工场；车间；研讨会
+mention#提及
+address#地址（名词）；处理（动词）
+climate#气候
+invention#发明
+
+remind#提醒
+
+tear#眼泪
+expectation#期待（名词）
+complaint#抱怨
+limit#限制
+beloved#首喜爱的
+senior#资深的；高年级的
+assistant#助手
+supply#供应
+sideline#边线
+
+after all#毕竟
+heater#加热器；暖气
+load#加载；装上
+truck#卡车
+truckload#满卡车的；一卡车的（量）
+stove#炉子
+depend on...#依靠
+brought#bring的过去式
+nothing to do with X#和X什么没有任何关系
+drive#驾驶；驱使
+firewood#柴火（劈好用来烧火的木头）
+
+rock#岩石
+discovery#发现（名词）
+disease#疾病
+development#发展（名词）
+publish#出版
+article#文章
+consider#考虑
+review#重新查看；审查；
+
+point#点、观点、分数（名词）；指向（动词）
+basically#基本上
+
+relate#相关的（形容词）；关联（动词）
+gain#得到
+reasonable#理智的；可被理解的
+inference#推导
+careless#粗心的
+store#商店；仓库（名词）；储存（动词）
+block#街区；区块（名词）；阻挡（动词）
+course#路线；课程
+rather than ...#而不是...
+interactive#互动的
+static#静止的；不变的
+effortlessly#无需努力地；轻松地
+prediction#预测
+familiarity#熟悉感（名词）
+come up with...#提出
+particular#特定的
+generalize#一般化
+compare#比较
+uncontrolled#不受控的
+unchanged#不变的
+
+constantly#不断地；经常地
+beg#恳求；乞讨
+apply#申请；应用（动词）
+responsibility#责任（名词）
+account#账户
+seriously#严重地；认真地
+
+thinker#思想家
+disagreement#分歧
+exactly#确切地
+dictionary#词典
+object#物体；东西；目标
+image#图片；想象
+etc#and so forth(et cetera)
+express#表达；快递
+be regarded...#被看做是...
+sculpture#雕塑
+concept#概念
+conceptual#概念的
+product#产品
+
+furniture#家具
+drama#戏剧
+function#功能
+functional#功能的；实用的
+
+as well as...#并且
+
+stool#凳子
+mount#安装上（动词）；山峰（名词）
+involvement#参与
+cover#盖子（名词）；盖上（动词）
+entrance#入口；进入（名词）
+entrance fee#入场费
+ordinary#平凡的
+goal#目标；射门
+
+plenty of...#许多
+
+belief#n. 信念；信仰；相信
+negative effect#负面效果
+meansure#测量
+brightness#亮度
+sensitive#敏感的
+
+protein#蛋白质
+
+wavelength#波长
+provide#提供
+troublesome#麻烦的
+pattern#模式；图案
+reduce#减少
+oppose#反对；对抗
+produce#生产；产生
+equally#相等地
+
+signal#信号
+view#n. 视野；见解；检查；v. 看；
+misguide#误导
+cooler#更凉的
+ensure#确保
+feature#特征；特性
+proper#合适的
+visual#视觉的
+aim#目标
+promote#促进；提升
+in line with...#和...保持一致
+schedule#时间表、日程（名词）；安排（动词）
+method#方法
+harmful#有害的
+balance#平衡
+
+decisive#决定性的；果断的
+confidence#信心
+self#自己
+self-confidence#自信
+
+doubt#怀疑
+curious#好奇
+strict#严格的
+whatever#无论什么；任何事物
+continuously#持续地
+please#请；使...高兴
+escape#逃离
+argue#争论
+
+disorder#混乱
+direct#直接的（形容词）；对准（动词）
+task#任务
+
+predict#预测
+wide#宽的
+long-term#长期
+will#将会（动词）；意志（名词）
+academy#学院
+Finland#芬兰
+face#脸（名词）；面对（动词）
+professor#教授
+achievable#可实现的；可达到的；
+fail#失败
+setback#挫败
+discourage#使...气馁（něi）
+draw#画；拉
+pay off#取得成功；得到回报
+courage#勇气
+
+hangout#巢穴
+favorable#讨人喜欢的
+eager#热切的
+eagerly#热切地
+bite#咬
+cast#抛
+jerk#急拉
+comfort#安慰（动词/名词）
+promise#承诺（动词）；誓言（名词）
+refit#重新安装
+once more#再一次
+show off#炫耀
+scene#场景
+directly#直接地
+
+disaster#灾难
+affect#影响
+staff#员工
+
+fiction#小说
+science fiction#科幻小说
+explore#探险
+factory#工厂
+surface#表面
+chore#琐事
+ability#能力
+result#结果
+
+complicated#复杂的
+pretty#漂亮的；非常（= very）
+for example#举个例子
+
+fill with...#装满了...
+in short#总之；简而言之
+variable#可变的（形容词）；变量（名词）
+turn out#结果是...
+underway#正在进行中
+purpose#目的
+hardly#几乎不
+
+statement#声明（名词）
+single parent#单亲（妈妈）
+gently#温柔地
+grateful#感激的
+ungrateful#不感激的
+wound#伤口（名词）；伤害（动词）
+privately#私密地
+win back#赢回来
+
+admire#羡慕
+
+text#文本、教科书、短信、发短信。
+upset#沮丧
+announce#宣布
+throughout#遍及
+carriage#车厢
+couple#夫妇；一对
+none#没有人；一个也没有；决不
+
+unless#除非
+otherwise#另外
+upon#on
+after...#在...之后
+pace#速度；步伐
+keep pace with...#和...保持步伐一致。（跟上）
+insist#坚持
+session#会议；一段时间
+shot#射击；拍摄；投篮
+respect#尊重
+direction#方向
+decision#决定（名词）
+competition#竞争；比赛（名词）
+praise#表扬
+advice#建议；忠告
+device#设备
+since then#从此之后
+
+silence#沉默
+conversation#谈话
+
+willpower#意志力
+platform#平台；站台
+grammar#语法
+electronic devices#电子设备
+electronic#电的；电子的
+
+super#超级
+interrupt#打断；打扰
+tryout#尝试；测验
+part#部分；角色
+
+obvious#明显的；显然的
+purposely#故意地
+plate number#车牌号
+confrontational#对抗；冲突
+sidewalk#人行道
+
+employee#员工
+significant#重要的；有意义的；
+determination#决心
+attitude#态度
+bar#酒吧；吧台
+application#申请；应用（名词）
+neat#整洁的；
+manager#经理
+failure#失败（名词）
+employ#雇佣
+elementary#基础
+elementary school#小学
+occasionally#偶尔地
+prove#证明
+satisfy#使满意；满足
+disappoint#失望
+
+accident#事故；意外
+disabled#残疾
+disable#使...失去能力
+conclude#总结
+
+suppose#假定；应该
+bag#包裹
+examine#检查；考试
+whenever#无论何时，任何时候
+phrase#短语
+selflessness#无私
+remember#记得；回忆
+see#看；明白
+
+spread#传播；扩散
+set off#出发
+left#左边；离开；剩下
+metal#金属
+material#材料
+capsule#胶囊；太空舱
+transport#交通
+freely#自由地
+turn#转（动词）；轮次（名词）
+stand for...#代表...
+slow...down#让...慢下来。
+advantage#优势；有利条件
+right now#now
+
+yard#院子；码（长度单位）
+either ... or ... #要么...要么...
+gun#枪
+strength#力量；特长
+
+thumb#拇指
+thumbs up#竖起大拇指
+awful#糟糕的
+expression#表情；表达
+
+equip#装备
+oxygen#氧气
+oxygen mask#氧气面具
+case#例子；情况；箱
+summit#顶峰
+fix#修理；固定
+ski#滑雪
+resort#度假胜地
+hike#远足；徒步旅行
+entire#全部的；整个的
+further#更远；更进一步
+endurance#忍耐
+plus#加上
+workout#训练
+mental#内心的
+mental strength#内心的力量
+condition#条件
+list#清单（名词）；罗列（动词）
+award#奖项
+
+devote#vt. 致力于；奉献给；
+write#写；写作；写信
+meanwhile#同时
+surprise#惊喜（名词）；使...惊喜（动词）
+positive#正面的；积极的
+once#曾经；一次；一旦
+look for#寻找
+look forward to...#期待
+lift#举起；提升
+decorate#装饰
+judge#判断；判决
+satellite#卫星
+industry#工业
+medium#中等的；适中的
+
+perform#执行；演出
+racket#球拍
+get used to doing...#习惯于做...
+no longer#不再
+hop#跳
+headmaster#校长
+microphone#麦克风
+not have to#不必
+carry on#go on = 继续
+audience#观众
+worry#担心
+
+perfect#完美
+gradually#渐渐地
+natrual#自然的
+pride#骄傲；自豪
+memory#记忆；回忆
+admiration#admire的名词；羡慕；赞美
+raise money#筹钱；募集善款
+edge#边缘
+a couple of...#一些...
+farther#更远
+anyone else#其他任何人
+
+letter#字母；信件
+smelly#臭烘烘的
+fatty#胖乎乎的
+prefer to...#偏爱
+flight#飞行；航班
+fear#害怕
+belt#带子；安全带
+put up with ...#容忍...
+rest#剩余部分；休息
+restore#恢复
+build#建造；体格
+
+subject#话题；科目
+goodness#天哪
+shell#外壳
+ancient#古老的
+atrractive#吸引人的
+easy#轻松的；容易的
+oversee#监视
+
+purse#钱包
+catch up with#赶上
+unexpectedly#意外地
+amaze#吃惊
+seldom#很少
+bully#欺负
+bravery#勇敢
+kindness#仁慈；好意
+permission#允许
+
+picky#挑剔的
+manner#行为举止
+unwillingly#不情愿地
+expect#期待
+sink#下沉
+stomach#胃；肚子
+scoop#勺子；舀取
+
+present#展现；现在；礼物
+
+rest of ...
+#
+...的剩余部分
+/
+rest of time
+rest of world
+rest of life
+reset of way
+
+leave#离开；剩下
+lace#蕾丝；鞋带
+alarm#警报；闹钟
+mind#心灵，心理（名词）；介意（动词）
+question#问题（名词）；质疑（动词）
+expense#n. 费用；花费；代价
+one of ...#...之一
+
+director#总监；导演
+fee#费用
+wrinkle#皱纹（名词）；起皱（动词）
+shiver#颤抖
+hire#雇佣
+
+priceless#无价的
+wrist#手腕
+gather#集中；集合
+gather courage#鼓起勇气
+precious#珍贵的
+anniversary#周年
+replace#替代；替换
+own up#承认
+
+inspiration#灵感；启发
+evident#明显的
+alert#警觉；警报
+surrounding#环境
+snap#啪一声；拍照；快照
+struggle#奋斗
+situation#情况；状况
+indeed#真正的；事实上
+determine#决定；决心
+in addition#另外；此外
+incapable#没有能力的
