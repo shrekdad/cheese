29f1154cf7f65d9af5e35d88bd7b4514eefd2d82
shrekdad <shrekdad@gmail.com>
2022-07-20 00:09:15 +0800

    Create projlist.py

---
+++ projlist.py
@@ -0,0 +1,8 @@
+
+github-eng-w#https://raw.githubusercontent.com/shrekdad/cheese/main/eng-w.py
+github-eng-g#https://raw.githubusercontent.com/shrekdad/cheese/main/eng-g.py
+
+github-chn-b#https://raw.githubusercontent.com/shrekdad/cheese/main/chn-b.py
+github-chn-r#https://raw.githubusercontent.com/shrekdad/cheese/main/chn-r.py
+
+github-math#https://raw.githubusercontent.com/shrekdad/cheese/main/math.py
